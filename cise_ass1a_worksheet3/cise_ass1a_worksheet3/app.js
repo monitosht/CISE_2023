214f5decb54b2457531853c07e38d7a4cb902bd8
monitosh <thaker.moni@gmail.com>
2023-08-12 21:44:32 +1200

    Connected to MongoDB

--- cise_ass1a_worksheet3/cise_ass1a_worksheet3/app.js
+++ cise_ass1a_worksheet3/cise_ass1a_worksheet3/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+// Connect to database
+
+const connectDB = require('./config/db');
+connectDB();
+
 app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 8082;
